56c4df710d9f5e3503d71909ba859c5b5fdefd94
Cydmi <19652109@qq.com>
2024-12-30 14:39:47 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '680933e1-d189-49e1-b990-34d586b4380b';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
